d78a5f2289dabcd67ac4774ea8a35925104cc9cc
Yosua Kristianto (Linus) <yosua_kristianto144@outlook.com>
2025-12-20 01:08:36 +0700

    Optimize import

--- nma-service/src/main/java/com/devcraftlabs/notification/model/entity/PersonalNotification.java
+++ nma-service/src/main/java/com/devcraftlabs/notification/model/entity/PersonalNotification.java
@@ -6,7 +6,6 @@ import java.util.UUID;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import lombok.Data;
 
