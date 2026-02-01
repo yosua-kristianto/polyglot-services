b0695cad8d39a129b55f242fa3f54265c50da029
Yosua Kristianto (Legion) <yosua_kristianto144@outlook.com>
2026-02-01 15:59:01 +0700

    Init KYC Service

---
+++ kyc-service/src/main/java/com/devcraftlabs/kyc/model/entity/lov/MaritalStatus.java
@@ -0,0 +1,28 @@
+package com.devcraftlabs.kyc.model.entity.lov;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "cma_tbl_lov_customer_marital_status")
+@Data
+public class MaritalStatus {
+
+    @Id
+    @Column(name = "marital_status_id", nullable = false, length = 20)
+    private String maritalStatusId;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt = Instant.now();
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Column(name = "deleted_at")
+    private Instant deletedAt;
+}
