9acb54ddd3a13bed6c6452918e151419ce2275a0
Yosua Kristianto (Linux) <yosua_kristianto144@outlook.com>
2026-02-12 14:09:17 +0700

    Backup

--- kyc-service/src/main/java/com/devcraftlabs/kyc/api/kyc/KYCController.java
+++ kyc-service/src/main/java/com/devcraftlabs/kyc/api/kyc/KYCController.java
@@ -1,5 +1,12 @@
 package com.devcraftlabs.kyc.api.kyc;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/kyc")
 public class KYCController {
     
+    
+
 }
--- kyc-service/src/main/java/com/devcraftlabs/kyc/api/kyc/KYCService.java
+++ kyc-service/src/main/java/com/devcraftlabs/kyc/api/kyc/KYCService.java
@@ -1,5 +1,36 @@
 package com.devcraftlabs.kyc.api.kyc;
 
-public class KYCService {
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.devcraftlabs.kyc.model.entity.Customer;
+
+@Service
+public class KYCService implements IKYCService {
+
+    @Override
+    public void submitKYCApplicationOCR(String fileUri) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'submitKYCApplicationOCR'");
+    }
+
+    @Override
+    public void submitKYCApplication(Customer customer) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'submitKYCApplication'");
+    }
+
+    @Override
+    public void approveKYCApplication(UUID kycId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'approveKYCApplication'");
+    }
+
+    @Override
+    public void rejectKYCApplication(UUID kycId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'rejectKYCApplication'");
+    }
     
 }
